786102f6f872b273e76a61b62e48600178a10647
Tejaswini Sahuji <tejusahuji@gmail.com>
2025-07-18 11:40:10 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,16 @@
+# TASK 1
+a = float(input("enter 1st number : "))
+b = float(input("enter 2nd number : "))
+sum = a + b
+print("Addition : ", sum)
+sub = a - b
+print("substraction : ", sub)
+mut = a * b
+print("multiplication: ", mut)
+div = a / b
+print("division : ", div)
+
+# TASK 2
+a = input("Enter your first name : ")
+b = input("Enter your last name : ")
+print("Hello ", a + b , "welcome to the python program")
